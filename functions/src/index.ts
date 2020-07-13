123b1d380d38066824e51019e0c5d31aee3165ce
asaf-horvitz <asaf.horvitz@gmail.com>
2020-07-13 13:01:13 +0300

    add test function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,20 @@
-import * as functions from 'firebase-functions';
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access Cloud Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+
+
+
+// Take the text parameter passed to this HTTP endpoint and insert it into 
+// Cloud Firestore under the path /messages/:documentId/original
+exports.addMessageToScreen = functions.https.onRequest(async (req : any, res: any) => {
+    // Grab the text parameter.
+    //const original = req.query.text;
+    // Push the new message into Cloud Firestore using the Firebase Admin SDK.
+    //const writeResult = await admin.firestore().collection('messages').add({original: original});
+    // Send back a message that we've succesfully written the message
+    res.json({result: `Message with ID XXX`});
+  });
