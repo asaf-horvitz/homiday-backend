68f71f63ee4e6c4847d9b2c5b82121865f2db595
asaf-horvitz <asaf.horvitz@gmail.com>
2020-08-16 13:02:03 +0300

    sample version

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,22 +1,33 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
 
-// The Firebase Admin SDK to access Cloud Firestore.
-const admin = require('firebase-admin');
-admin.initializeApp();
+import * as functions from 'firebase-functions'
 
-let a = 5
-console.log(a);
-console.log('12312312312');
+// if you need to use the Firebase Admin SDK, uncomment the following:
+// import * as admin from 'firebase-admin'
 
 
-// Take the text parameter passed to this HTTP endpoint and insert it into 
-// Cloud Firestore under the path /messages/:documentId/original
-exports.addMessageToScreen = functions.https.onRequest(async (req : any, res: any) => {
-    // Grab the text parameter.
-    //const original = req.query.text;
-    // Push the new message into Cloud Firestore using the Firebase Admin SDK.
-    //const writeResult = await admin.firestore().collection('messages').add({original: original});
-    // Send back a message that we've succesfully written the message
-    res.json({result: `Message with ID XXX`});
-  });
+// Create and Deploy Cloud Function with TypeScript using script that is
+// defined in functions/package.json:
+//    cd functions
+//    npm run deploy
+
+console.log('555');
+
+export const helloWorld2 = functions.https.onRequest((request, response) => {
+ response.send('Hello from Firebase!\n\n');
+});
