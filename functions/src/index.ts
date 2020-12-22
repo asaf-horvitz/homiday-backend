8663127f286ab60ad5251927d989acc2c18a3fc9
asaf-horvitz <asaf.horvitz@gmail.com>
2020-12-22 11:43:03 +0200

    notification for new exchange msg

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -76,3 +76,55 @@ exports.updateProfile = functions.firestore
     console.log(ex);         
   }
 })
+
+
+export const sendNotificationToFCMToken2 = functions.firestore.document('production/production/msgs/msgs/exchange-msgs/{mUid}').onWrite(async (change, context) => {
+  try{
+    const admin = require('firebase-admin');
+    
+    const to = change.after.get('to');
+    const from = change.after.get('from');
+    console.log(change.after.data() );
+    console.log('from : ' + from );
+    console.log('to : ' + to );
+    console.log('mUid : ' + context.params.mUid);
+    
+    var title = 'msg from ' + from;
+    var content ='Exchange msg';
+
+    const confirm = change.after.get('confirm');
+    const canceled = change.after.get('canceled');
+
+    if (confirm == 'true'){
+      console.log('conifirm : True');
+      content = 'Exchange msg Confirm! ';
+    }
+
+    if (canceled == 'true'){
+      console.log('Canceled : True');
+      content = 'Exchange msg Canceled! ';
+    }
+    
+    let userDoc = await admin.firestore().doc(`production/production/notification-tokens/${to}`).get();
+    let fcmToken = userDoc.get('token');
+    console.log('fcm : ' + fcmToken);
+    var message = {
+        notification: {
+            title: title,
+            body: content,
+        },
+        data: {
+          click_action: 'FLUTTER_NOTIFICATION_CLICK',
+        },
+        token: fcmToken,
+
+    }
+
+    let response = await admin.messaging().send(message);
+    console.log(response);
+
+}
+catch (ex) {
+    console.log(ex);         
+  }
+});
